055283a7edad2f3142a2075aa2dd5b6bb7261070
Vishwajit Kolathur <aj.ajit@gmail.com>
2013-01-01 16:31:41 +0530

    Updated install.sh file

--- coursesync/install.sh
+++ coursesync/install.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
-DIR=$(pwd)
-get_cpanm(){
-    if [ \! -f /usr/local/bin/cpanm ]; then
-		cd $TMP_DIR && curl --insecure -L http://cpanmin.us | perl - App::cpanminus
-		if [ \! -f /usr/local/bin/cpanm ]; then
-		echo "Downloading from cpanmin.us failed, downloading from xrl.us"
-		curl -LO http://xrl.us/cpanm &&
-		chmod +x cpanm &&
-		mv cpanm /usr/local/bin/cpanm
-		fi
-	fi
-	CPANM=$(which cpanm);
-	if [ \! -f "$CPANM" ]; then
-		echo "ERROR: Unable to find cpanm"
-		return 1;
-	fi
-	return 0
-}
-get_cpanm
-sudo cpanm LWP::Simple URI::Escape File::HomeDir WWW::Mechanize
-cd $DIR
+sudo perl -MCPAN -e 'install LWP::Simple'
+sudo perl -MCPAN -e 'install URI::Escape'
+sudo perl -MCPAN -e 'install Fle::HomeDir'
+sudo perl -MCPAN -e 'install WWW::Mechanize'
 chmod +x script.pl
 sudo cp -p script.pl /usr/local/bin/CourseSync
-sudo install -g 0 -o 0 -m 0644 .CourseSync.1 /usr/local/share/man/man1/CourseSync.1
+sudo cp .CourseSync.1 /usr/share/man/man1/CourseSync.1
